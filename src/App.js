a772b2ab6534853bb95e5d8bbcedc6637f8bd445
sungwoo <tjddn5242@gmail.com>
2020-08-10 16:01:27 +0900

    second

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function WorldClock(props) {
-  return (
-    <div className={"WorldClock"}>
-      <h2>🌎: {props.city}</h2>
-      <p>🕐: {props.time}</p>
-    </div>
-  )
+//하위컴포넌트
+// function WorldClock(props) {
+//   return (
+//     <div className={"WorldClock"}>
+//       <h2>🌎: {props.city}</h2>
+//       <p>🕐: {props.time}</p>
+//     </div>
+//   )
+// }
+
+class WorldClock extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      hour: this.props.time,
+      minute: 0
+    }
+    //state 바꾸기
+    setInterval(() => {
+      this.setState((state) => (
+        state.minute === 59
+        ? {hour: state.hour + 1, minute: 0}
+        : {minute: state.minute + 1}      
+      ))
+    },100)
+
+  }
+//render라는 미리 약속된 함수
+  render() {
+    return (
+      <div className={"WorldClock"}>
+        <h2>🌎: {this.props.city}</h2>
+        <p>🕐: {this.state.hour}시 {this.state.minute}분</p>
+      </div>
+    )
+
+  }
 }
 
 
-//JSX사용
+//상위컴포넌트
 function App() {
   const cityTimeData = [
     ['서울', 10],
@@ -21,13 +52,13 @@ function App() {
     ['LA', 17],
     ['부산', 10]
   ]
-
+  //props
   const worldClockList = cityTimeData.map((citytime, index)=>
     <WorldClock city={citytime[0]} time={citytime[1]} key={index}/>
   )
   return (
     <div className="App">
-      <h1 className={'myStyle'}>Welcome Judy!</h1>
+      <h1 className={'myStyle'}>Welcome!</h1>
       <div className={'post'}>
         🌎Time
       </div>
